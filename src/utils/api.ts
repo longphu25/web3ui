e95cb5e724840bdcf871622d070e755745ebd84c
Long Phu <phu.huynh@monimedia.com>
2023-11-16 18:03:12 +0700

    Check call server with http

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,12 +2,12 @@ import axios, { AxiosInstance } from 'axios'
 
 const instance: AxiosInstance = axios.create({
   // Set your base URL
-  baseURL: 'http://178.63.14.81:21558',
+  baseURL: 'https://web3school.fun:21558',
   // baseURL: 'https://runner-algo.web3school.fun/',
   // Disable SSL certificate validation (use with caution)
-  httpsAgent: {
-    rejectUnauthorized: false,
-  },
+  // httpsAgent: {
+  //   rejectUnauthorized: false,
+  // },
 })
 
 export default instance
